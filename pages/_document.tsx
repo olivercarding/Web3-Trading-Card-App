ff7eca933dec90c8088a96a6af1b62a14469aad5
Oliver Carding <olivercarding@mac.com>
2023-10-02 13:57:28 +0100

    Ready to build/ Export
    Latest version

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+    // pages/_document.tsx
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        <Title> CryptoKaiju Cards - Redeemable Vinyl Toys</Title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
